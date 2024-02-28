66f5a2418c3a764c2d7e49fd92637c9854f2a0b8
Leonardo Sasso <ljscodex@gmail.com>
2024-02-27 23:13:02 -0300

    more challenges solves, more classes

---
+++ ForCodingChallenges/LeetCode/LeetCode_BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCodingChallenges.LeetCode
+{
+    internal class LeetCode_BinarySearch
+    {
+        //35. Search Insert Position
+        public int SearchInsert(int[] nums, int target)
+        {
+
+            List<int> Index = nums.ToList();
+            int i = Index.IndexOf(target);
+            if (i >= 0) { return i; }
+
+            int o = Index.FindIndex(x => x > target);
+            int s = Index.FindIndex(x => x < target);
+
+            if (o > s) { return o; }
+            if (o == -1 && s <= 0) { return nums.Length; }
+
+            return 0;
+
+        }
+
+    }
+}
---
+++ ForCodingChallenges/LeetCode/LeetCode_Math.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ForCodingChallenges.LeetCode
+{
+    internal class LeetCode_Math
+    {
+        //50. Pow(x, n)
+        public double MyPow(double x, int n)
+        {
+            return Math.Pow(x, n);
+        }
+
+        //69. Sqrt(x)
+
+        public int MySqrt(int x)
+        {
+
+            return (int)Math.Sqrt(x);
+        }
+
+        //66. Plus One
+        public int[] PlusOne(int[] digits)
+        {
+            string number = String.Join("", digits);
+            BigInteger numbers = BigInteger.Parse(number) + 1;
+            digits = numbers.ToString("0." + new string('#', digits.Length)).Select(t => int.Parse(t.ToString())).ToArray();
+            return digits;
+        }
+
+        //9,223,372,036,854,775,807
+        //7,285,091,295,366,732,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6
+
+        //9. Palindrome Number
+        public bool IsPalindrome(int x)
+        {
+            char[] tmp = x.ToString().ToCharArray();
+
+            for (int a = 0; a < tmp.Length - 1; a++)
+            {
+                if (tmp[a] != tmp[tmp.Length - 1 - a])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
